532743ae468a535fdb1a270aabbe773122d35098
Oswaldo052001 <3014167370101@ingenieria.usac.edu.gt>
2023-12-25 10:12:15 -0600

    Inicio de la fase 2

---
+++ Fase2/backend/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"Fase2/estructuras"
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+var arbolitoB *estructuras.ArbolB = &estructuras.ArbolB{Raiz: nil, Orden: 3}
+
+func AgregarB(c *fiber.Ctx) error {
+	fmt.Println("Recibi Solicitud")
+	var nuevoElemento estructuras.SolicitudArbolB
+	err := c.BodyParser(&nuevoElemento)
+	if err != nil {
+		return c.JSON(&fiber.Map{
+			"status":  400,
+			"message": "Error al ingresar ",
+		})
+	}
+	arbolitoB.Insertar(nuevoElemento.Valor)
+	return c.JSON(&fiber.Map{
+		"status":  200,
+		"message": "Valor ingresado",
+	})
+}
+
+func GenerarReporte(c *fiber.Ctx) error {
+	fmt.Println("Recibi solicitud")
+	var reporte estructuras.SolicitudReporte
+	c.BodyParser(&reporte)
+	if reporte.Estructura_solicitada == "Arbol B" {
+		arbolitoB.Graficar(reporte.Nombre)
+		return c.JSON(&fiber.Map{
+			"status":  200,
+			"message": "Grafica Generada",
+		})
+	} else {
+		return c.JSON(&fiber.Map{
+			"status":  400,
+			"message": "Error",
+		})
+	}
+}
+
+func main() {
+	app := fiber.New()
+	app.Post("/ingresar-arbol-b", AgregarB)
+	app.Post("/reporte", GenerarReporte)
+	app.Listen(":4000")
+}
