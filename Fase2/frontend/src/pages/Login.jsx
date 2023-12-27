dd9527a1b34d5baecf5d9e7320782d8e189009cb
Oswaldo052001 <3014167370101@ingenieria.usac.edu.gt>
2023-12-26 18:24:22 -0600

    Creación de Login

--- Fase2/frontend/src/pages/Login.jsx
+++ Fase2/frontend/src/pages/Login.jsx
@@ -2,7 +2,55 @@ import React from 'react'
 
 function Login() {
   return (
-    <div><h1>LOGIN</h1></div>
+
+<section classname="text-center text-lg-start">
+  <style  dangerouslySetInnerHTML={{__html: "\n      .cascading-right {\n        margin-right: -50px;\n      }\n  \n      @media (max-width: 991.98px) {\n        .cascading-right {\n          margin-right: 0;\n        }\n      }\n    " }} />
+  {/* Jumbotron */}
+  <div className="container py-4">
+    <div className="row g-0 align-items-center">
+      <div className="col-lg-6 mb-5 mb-lg-0">
+        <div className="card cascading-right" style={{background: 'hsla(0, 0%, 100%, 0.55)', backdropFilter: 'blur(30px)'}}>
+          <div className="card-body p-5 shadow-5 text-center">
+            <h2 className="fw-bold mb-5">Inicio de sesión</h2>
+            <form>
+              {/* Email input */}
+              <div className="form-outline mb-4">
+                <input type="email" id="form3Example3" className="form-control" />
+                <label className="form-label" htmlFor="form3Example3">Usuario</label>
+              </div>
+              {/* Password input */}
+              <div className="form-outline mb-4">
+                <input type="password" id="form3Example4" className="form-control" />
+                <label className="form-label" htmlFor="form3Example4">Contraseña</label>
+              </div>
+
+              {/* Submit button */}
+              <button type="submit" className="btn btn-primary btn-block mb-4">
+                Iniciar sesión
+              </button>
+              
+              {/* Submit button */}
+
+              <div className="form-outline mb-4">
+              <button type="submit" className="btn btn-dark">
+                Portal tutor
+              </button>
+              </div>
+
+
+            </form>
+          </div>
+        </div>
+      </div>
+      <div className="col-lg-6 mb-5 mb-lg-0">
+        <img src="https://mdbootstrap.com/img/new/ecommerce/vertical/004.jpg" className="w-100 rounded-4 shadow-4" alt />
+      </div>
+    </div>
+  </div>
+  {/* Jumbotron */}
+</section>
+
+
   )
 }
 
